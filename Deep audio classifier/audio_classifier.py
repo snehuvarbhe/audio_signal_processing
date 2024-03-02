afd7745432d71e20a65fd23d3c45e4d8e3014d9f
snehal <snehuvarbhe26@gmail.com>
2024-03-02 13:22:00 +0530

    commit

--- Deep audio classifier/audio_classifier.py
+++ Deep audio classifier/audio_classifier.py
@@ -1,1 +1,11 @@
+# Import libraries
 import numpy as np
+import matplotlib.pyplot as plt
+import os
+import tensorflow as tf
+from  tensorflow import io as tfio
+
+# Build data loading functions
+# path to file
+capuchin=os.path.join('data','Parsed_Capuchinbird_Clips','XC3776-0.wav')
+capuchin
--- preprocessing_signal.py
+++ preprocessing_signal.py
@@ -4,12 +4,12 @@ import numpy as np
 
 file="blues.00000.wav"
 
-# Display time domain waveform
-
+#Display time domain waveform
 signal,sr=librosa.load(file,sr=22050)
 librosa.display.waveshow(signal, sr=sr, color='pink')
 plt.xlabel("Time")
 plt.ylabel("Amplitude")
+plt.title("Signal in time domain")
 plt.show()
 
 #fft spectrum
@@ -17,4 +17,57 @@ fft=np.fft.fft(signal)
 magnitude=np.abs(fft)
 frequency=np.linspace(0,sr,len(magnitude))
 plt.plot(frequency,magnitude)
+plt.xlabel("Frequency")
+plt.ylabel("Magnitude")
+plt.title("Full fft signal")
 plt.show()
+
+#In the above spectrum we have a mirror image spectrum output where only one side frequncy is important to display signal
+left_frequency=frequency[:int(len(frequency)/2)]
+left_magnitude=magnitude[:int(len(frequency)/2)]
+plt.plot(left_frequency,left_magnitude)
+plt.xlabel("Frequency")
+plt.ylabel("Magnitude")
+plt.title("Left fft signal")
+plt.show()
+
+#The above signal gives static information about signal and to know dynamic imformation we perform stft_>spectrogram
+#It gives information of signal in both frequency and time domain
+n_fft=2048 #number of samples basically the window to perform single ft
+hop_length=512 #amount we shift to perform next ft
+
+stft=librosa.core.stft(signal,hop_length=hop_length,n_fft=n_fft)
+spectrogram=np.abs(stft)
+librosa.display.specshow(spectrogram,sr=sr,hop_length=hop_length)
+plt.colorbar(format='%+2.0f dB') 
+plt.xlabel("Time")
+plt.ylabel("Frequency")
+plt.title("spectrogram")
+plt.show()
+
+#log spectrogram
+log_spectrogram=librosa.amplitude_to_db(spectrogram)
+librosa.display.specshow(log_spectrogram,sr=sr,hop_length=hop_length)
+plt.colorbar(format='%+2.0f dB') 
+plt.xlabel("Time")
+plt.ylabel("Frequency")
+plt.title("log spectrogram")
+plt.show()
+
+#MFCC
+mfcc=librosa.feature.mfcc(y=signal,n_fft=n_fft,hop_length=hop_length,n_mfcc=13)
+librosa.display.specshow(mfcc,sr=sr,hop_length=hop_length)
+plt.xlabel("Time")
+plt.ylabel("mfcc")
+plt.title("mfcc")
+plt.show()
+
+
+
+
+
+
+
+
+
+
